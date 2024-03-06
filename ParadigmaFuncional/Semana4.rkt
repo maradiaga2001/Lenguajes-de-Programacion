8c1d3909e3083151695588f9ba543bb0ff58d298
maradiaga2001 <113129283+maradiaga2001@users.noreply.github.com>
2024-03-06 09:54:42 -0600

    Update Semana4.rkt

--- ParadigmaFuncional/Semana4.rkt
+++ ParadigmaFuncional/Semana4.rkt
@@ -1,1 +1,14 @@
+;Ejercicio2
+(define (contieneSubcadena? cadena subcadena)
+  (let loop ((i 0))
+    (cond ((>= (+ i (string-length subcadena)) (string-length cadena)) #f)
+          ((string=? (substring cadena i (+ i (string-length subcadena))) subcadena) #t)
+          (else (loop (+ i 1))))))
 
+(define (filtrar listaCadenas subcadena)
+  (filter (lambda (cadena) (contieneSubcadena? cadena subcadena)) listaCadenas))
+
+; Para verificar funcionamiento usar lo siguiente:
+;(display (filtrar '("la casa," "el perro" "pintando la cerca") "la"))
+;(display (filtrar '("el carro," "la perra" "pintando el techo") "la"))
+;(display (filtrar '("el carro," "la perra" "pintando el techo") "el"))
